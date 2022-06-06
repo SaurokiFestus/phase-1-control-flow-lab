ebd9fb1cc5b84308aa17f3d17a46b1f51963ae0b
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-06-06 16:04:15 +0300

    commit tree 00eddc16b08227ea7d66ae46767e12e47be0afd5

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(val){
   // Write your code here!
+  if (val <= 400){
+    return "This one is on me!";
+  }else if (val >= 2000 && val <=2500){
+    return "I will gladly take your thirty bucks."
+  }else if (val >2500){
+    return "No can do."
+  }
+  else {
+    return "Pay for it now!"
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+  }
